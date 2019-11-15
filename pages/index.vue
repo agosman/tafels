3aad77f9517e96fe96e23a0fcf5d5865855b6473
Arjen Gosman <arjen@born05.com>
2019-11-15 15:28:59 +0100

    First commit

---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <div>
+      <h2 class="subtitle">
+        De tafels
+      </h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+};
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+  @apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+</style>
